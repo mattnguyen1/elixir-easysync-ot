0c01b586852e5806fffb62e7f6b2a48e0c629223
mattnguyen1 <mattnguyen1@yahoo.com>
2018-02-18 18:20:40 -0800

    Made Changeset into a struct and added Changeset.Op module

--- lib/changeset.ex
+++ lib/changeset.ex
@@ -4,6 +4,15 @@ defmodule Changeset do
 	changesets for operational transforms.
 	"""
 
+	defstruct old_len: 0, new_len: 0, ops: "", char_bank: ""
+
+	@doc """
+	Returns an empty Changeset struct
+	"""
+	def new do
+		%Changeset{}
+	end
+
 	@doc """
 	Unpacks a changeset string into a changeset keyword list
 
@@ -23,27 +32,37 @@ defmodule Changeset do
 		ops = String.slice(cs, ops_start..ops_end-1)
 		char_bank = String.slice(cs, ops_end+1..-1)
 
-		[
-			{:old_len, old_len}, # length of text before changeset is applied
-			{:new_len, new_len}, # length of text after changeset is applied
-			{:ops, ops}, # change operations on the text
-			{:char_bank, char_bank} # characters added in the change
-		]
+		%Changeset{
+			old_len: old_len, # length of text before changeset is applied
+			new_len: new_len, # length of text after changeset is applied
+			ops: ops, # change operations on the text
+			char_bank: char_bank # characters added in the change
+		}
 	end
 
 	@doc """
-	Packs a changeset keyword list into a string
+	Packs a Changeset into a string
 
 	## Examples
 
 		iex> Changeset.pack([{:old_len, 1},{:new_len, 4},{:ops, "=1*0+3"},{:char_bank, "hey"}])
-		"Z:1>3=1*0+3$hey"
+		"Z:1>3=1*0+3$hey"``
 	"""
-	def pack([{:old_len, old_len}, {:new_len, new_len}, {:ops, ops}, {:char_bank, char_bank}]) do
-		len_change = Integer.to_string(new_len - old_len)
+	def pack(cs) do
+		len_change = Integer.to_string(cs.new_len - cs.old_len)
 		change_sign = if len_change >= 0, do: ">", else: "<"
-		old_len = Integer.to_string(old_len)
+		old_len = Integer.to_string(cs.old_len)
 
-		"Z:" <> old_len <> change_sign <> len_change <> ops <> "$" <> char_bank
+		"Z:" <> old_len <> change_sign <> cs.len_change <> cs.ops <> "$" <> cs.char_bank
+	end
+end
+
+defmodule Changeset.Op do
+	defstruct opcode: "", chars: 0, lines: 0, attribs: ""
+
+	def new(opts \\ []) do
+		%Changeset.Op{
+			opcode: Keyword.get(opts, :opcode, "")
+		}
 	end
 end
