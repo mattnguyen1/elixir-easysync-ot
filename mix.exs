e20db01845ff6ef80b7eff4304a55f3cc1f2944a
mattnguyen1 <mattnguyen1@yahoo.com>
2018-02-19 19:24:06 -0800

    Added coveralls support

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule ElixirEasysyncOt.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+	  test_coverage: [tool: ExCoveralls]
     ]
   end
 
@@ -21,6 +22,7 @@ defmodule ElixirEasysyncOt.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+	{:excoveralls, github: "parroty/excoveralls"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
